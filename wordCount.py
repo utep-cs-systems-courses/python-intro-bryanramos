e1d7ef024ca324c9e60d9d674c8c03ccfe931e31
Bryan Ramos <ramos.bryan100@gmail.com>
2020-09-01 23:08:45 -0600

    removing redundant comments, whitespace

--- wordCount.py
+++ wordCount.py
@@ -3,7 +3,7 @@
 # Author: Bryan Ramos
 # Course: Theory of Operating Systems
 # Instructors: Eric Freudenthal and David Pruitt
-# Assignment: Lab Python Introduction
+# Assignment: Python Lab 1
 # Assignment Description: Count the number of words in a file. The program takes to file names as input
 # (arguments), reads the first, and then writes all word and the amount of times they occur to second file.
 
@@ -11,38 +11,39 @@ import os  # check if file exists
 import re  # for regular expression
 import sys # command line arguments
 
-# set input and output files and check if input is valid, if not terminate program
+# set input and output files and check if input is valid, if not terminate the program
 # 3 args necessary from system
-if len(sys.argv) != 3: 
-    print("Incorrect input. Correct input usage for this program: wordCount.py <input file> <output file> ")
+if len(sys.argv) != 3:
+    print("Incorrect input. Correct input usage for this program: wordCount.py <input file> <output file>")
     exit()
 
-inputFilename = sys.argv[1]  # save name of input file
-outputFileName = sys.argv[2] # save name of output file
-words = {}                   # python dictionary - where words & counts will be stored
+inputFilename = sys.argv[1]
+outputFileName = sys.argv[2]
+words = {} # python dictionary - where words & counts will be stored
 
-# check to make sure the input file exists
+# check to make sure the input file exists, if not terminate the program 
 if not os.path.exists(inputFilename):
-    print("The file %s does not exist!" % inputFilename);
+    print("The file %s does not exist!" % inputFilename)
     exit()
 
 with open(inputFilename, 'r') as thisFile:
-    for line in thisFile:                             # for every line in file
-        line = line.strip()                           # remove newline characters
-        for word in re.split("[\"'\s\t.,-;:]", line): # split at spaces, punctuation
+    for line in thisFile:
+        line = line.strip()                             # remove newline characters
+        for word in re.split("[\"'\s\t.,-;:?!]", line): # split at spaces, punctuation, tab
             if word.lower() == "":
                 continue
-            if word.lower() in words:                 # if word is already in words dict, add 1 to count for that
+            if word.lower() in words:                   # if word is already in words dict, add 1 to count for that
                 words[word.lower()] += 1
-            else:                                   
+            else:
                 words[word.lower()] = 1 
 
 # write to output file
-# w+ will overwrite the existing file if it exists
+# w+ will overwrite the existing file if it exists as per requirements
 file = open(outputFileName, "w+")
 
-# sort word dict and then start writing to file
+# sort word dict (per requirements the words in the output file should be in descending order) 
+# and then start writing to file
 for word, index in sorted(words.items()):
-    file.write(word + " " + str(index) + "\n") 
+    file.write(word + " " + str(index) + "\n")
 
 file.close() # close the output file when done
