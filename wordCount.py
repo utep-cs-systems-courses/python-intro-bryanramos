3ce405935b6293631edce2742304307b8bb21baf
Bryan Ramos <ramos.bryan100@gmail.com>
2020-09-01 22:01:37 -0600

    Adding code to count words and save to file

--- wordCount.py
+++ wordCount.py
@@ -14,5 +14,16 @@ if not os.path.exists(inputFilename):
 with open(inputFilename, 'r') as thisFile:
     for line in thisFile:                           # for every line in file
         line = line.strip()                         # remove newline characters
-        for word in re.split("['\s\t.,-;:]", line):
-            print(word);
+        for word in re.split("['\s\t.,-;:]", line): # split at spaces, punctuation
+            if word.lower() in words:               # if word is already in words dict, add 1 to count for that
+                words[word.lower()] += 1
+            else:                                   
+                words[word.lower()] = 1 
+
+# write to output file
+file = open(outputFileName, "w+")
+
+for word, index in sorted(words.items()):
+    file.write(word + " " + str(index) + "\n") 
+
+file.close()    
