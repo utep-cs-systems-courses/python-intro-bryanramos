a2bdd1d7bbb7d6457062324e47061480078dd202
Bryan Ramos <ramos.bryan100@gmail.com>
2020-09-01 21:12:06 -0600

    Added code for reading content from input file

--- wordCount.py
+++ wordCount.py
@@ -1,5 +1,6 @@
 import os  # check if file exists
 import sys # command line arguments
+import re  # for regular expression
 
 inputFilename = sys.argv[1]  # save name of input file
 outputFileName = sys.argv[2] # save name of output file
@@ -10,3 +11,8 @@ if not os.path.exists(inputFilename):
     print("The file %s does not exist!" % inputFilename);
     exit()
 
+with open(inputFilename, 'r') as thisFile:
+    for line in thisFile:                           # for every line in file
+        line = line.strip()                         # remove newline characters
+        for word in re.split("['\s\t.,-;:]", line):
+            print(word);
